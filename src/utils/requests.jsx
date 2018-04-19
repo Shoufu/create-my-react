540d49d856a86ec4921fab6604d76cbb7726809b
GYMing <gyming@outlook.com>
2018-04-19 10:58:13 +0800

    [update]更新请求方法

--- src/utils/requests.jsx
+++ src/utils/requests.jsx
@@ -1,36 +1,85 @@
 require('es6-promise').polyfill()
 require('isomorphic-fetch')
 
-const headers = {
-  'Content-Type': 'application/x-www-form-urlencoded'
+const headers = new Headers({
+  'Accept': '*/*',
+  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+  'X-Requested-With': 'XMLHttpRequest'
+})
+
+const uploadOptions = {
+  mode: 'no-cors',
+  credentials: 'include',
 }
 
-async function get(url) {
-  const response = await fetch(url, {
-    headers
-  })
-  return response.json()
+const options = {
+  ...uploadOptions,
+  headers,
 }
 
-async function post(url, data) {
-  const body = serialize(data)
-  const response = await fetch(url, {
-    method: 'POST',
-    headers,
-    body
-  })
-  return response.json()
-}
-
+/**
+ * 对表单的数据进行序列化
+ * @param {object} data 表单数据（注意类型不是 FormData）
+ */
 function serialize(data) {
-  const str = []
-  const keys = Object.keys(data)
-  if (!keys.length) return ''
-  keys.forEach(item => {
-    str.push(`${encodeURIComponent(item)}=${encodeURIComponent(data[item])}`)
-  })
+  return Object.keys(data).map(key => {
+    return `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+  }).join('&')
+}
 
-  return `${str.join('&')}`
+/**
+ * 将字符串转换成对象 Object，如果失败则返回该字符串
+ * @param {string} text 要转换的字符串
+ */
+function parse(text) {
+  try {
+    const data = JSON.parse(text)
+    return data
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      return text
+    } else {
+      throw e
+    }
+  }
+}
+
+/**
+ * 模拟 HTTP GET 方法
+ * @param {string} url 要访问的 url
+ */
+async function get(url) {
+  const response = await fetch(url, options)
+  const text = await response.text()
+  return parse(text)
+}
+
+
+/**
+ * 模拟 HTTP POST 方法
+ * @param {string}            url         要访问的 url
+ * @param {object|FormData}   data        要 POST 的数据
+ */
+async function post(url, data) {
+  let body, option
+  if (data instanceof FormData) {
+    body = data
+    option = uploadOptions
+  } else {
+    body = serialize(data)
+    option = options
+  }
+
+  const response = await fetch(
+    url,
+    Object.assign({
+      method: 'POST',
+      body
+    }, option)
+  )
+
+  const text = await response.text()
+  return parse(text)
 }
 
 export default { get, post }
