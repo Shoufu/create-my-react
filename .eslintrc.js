a9830814d2fdf54b70bf98aaa3275f2755f05320
GYMing <407886535@qq.com>
2018-06-28 17:51:15 +0800

    [update] 更新 eslint 检查风格为 standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ['standard', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
     },
-    "extends": "eslint:recommended",
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "no-console": process.env.NODE_ENV === 'production' ? 2 : 0,
-        "react/jsx-uses-react": "error",
-        "react/jsx-uses-vars": "error",
-        "indent": [
-            "error",
-            2,
-            { "SwitchCase": 1 }
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
-};
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'react/display-name': 0
+  }
+}
